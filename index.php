15d23eaaa3a7855e4463acbc70ed84ef7a9153e2
MR <admin@mr-cn.net>
2016-08-16 09:48:39 +0800

    Test1

--- index.php
+++ index.php
@@ -1,94 +1,74 @@
 <?php
+/**
+ * 贴吧云签到
+ * Copyright (c) 2012~2016 StusGame All Rights Reserved.
+ * 
+ * 获取开发文档：https://git.oschina.net/kenvix/Tieba-Cloud-Sign/wikis/
+ */
+require dirname(__FILE__).'/init.php';
+global $i;
 
-require('DB.php');
-
-$db = new DB();
-
-if ($_POST) {
-    $db->add($_POST['name'], $_POST['phone']);
+if (!isset($_GET['plugin']) && !isset($_GET['pub_plugin']) && !isset($_GET['vip_plugin']) && !isset($_GET['pri_plugin']) && $i['mode'][0] != 'view') {
+	template('control');
+	loadfoot();
+} elseif ($i['mode'][0] == 'view') {
+	if (in_array($i['mode'][1], $i['plugins']['actived']) && file_exists(SYSTEM_ROOT . '/plugins/' . $i['mode'][1] . '/' . 'view_' . $i['mode'][2] . '.php')) {
+		$plug = getPluginInfo($i['mode'][1]);
+		if (in_array($i['mode'][2], $plug['page'])) {
+			include SYSTEM_ROOT . '/plugins/' . $i['mode'][1] . '/' . 'view_' . $i['mode'][2] . '.php';
+		} else {
+			msg('无效的插件自定义页面，请在 插件名_desc.php 中的page键添加此页面');
+		}
+	} else {
+		ReDirect('index.php');
+	}
+} elseif (isset($_GET['plugin'])) {
+    $plug=strip_tags($_GET['plugin']);
+    if (in_array($plug, $i['plugins']['actived'])) {
+		if (file_exists(SYSTEM_ROOT.'/plugins/'.$plug.'/'.$plug.'_show.php') && !is_dir(SYSTEM_ROOT.'/plugins/'.$plug.'/'.$plug.'_show.php')) {
+			require_once SYSTEM_ROOT.'/plugins/'.$plug.'/'.$plug.'_show.php';
+		} else {
+			msg('插件前台显示模块不存在或不正确');
+		}
+	}
+} elseif (isset($_GET['vip_plugin'])) {
+	$plug=strip_tags($_GET['vip_plugin']);
+	if (in_array($plug, $i['plugins']['actived'])) {
+		if (file_exists(SYSTEM_ROOT.'/plugins/'.$plug.'/'.$plug.'_vip.php') && !is_dir(SYSTEM_ROOT.'/plugins/'.$plug.'/'.$plug.'_vip.php')) {
+			if (ISVIP) {
+				require_once SYSTEM_ROOT.'/plugins/'.$plug.'/'.$plug.'_vip.php';
+			} else {
+				msg('权限不足');
+			}
+		} else {
+			msg('插件前台显示模块不存在或不正确');
+		}
+	}
+} elseif (isset($_GET['pri_plugin'])) {
+	$plug=strip_tags($_GET['pri_plugin']);
+	if (in_array($plug, $i['plugins']['actived'])) {
+		if (file_exists(SYSTEM_ROOT.'/plugins/'.$plug.'/'.$plug.'_private.php') && !is_dir(SYSTEM_ROOT.'/plugins/'.$plug.'/'.$plug.'_private.php')) {
+			if (ROLE == 'admin') {
+				require_once SYSTEM_ROOT.'/plugins/'.$plug.'/'.$plug.'_private.php';
+			} else {
+				msg('权限不足');
+			}
+			require_once SYSTEM_ROOT.'/plugins/'.$plug.'/'.$plug.'_private.php';
+		} else {
+			msg('插件前台显示模块不存在或不正确');
+		}
+	}
+} elseif (defined('SYSTEM_READY_LOAD_PUBPLUGIN')) {
+	$i['user']['role'] = 'visitor';
+	$plug = strip_tags($_GET['pub_plugin']);
+	if (in_array($plug, $i['plugins']['actived'])) {
+		if (file_exists(SYSTEM_ROOT.'/plugins/'.$plug.'/'.$plug.'_public.php') && !is_dir(SYSTEM_ROOT.'/plugins/'.$plug.'/'.$plug.'_public.php')) {
+			require_once SYSTEM_ROOT.'/plugins/'.$plug.'/'.$plug.'_public.php';
+		} else {
+			msg('插件前台显示模块不存在或不正确');
+		}
+	} else {
+		ReDirect('index.php?mod=login');
+	}
+	die;
 }
-if ($_GET['delete']) {
-    $db->remove($_GET['delete']);
-}
-
-$contacts = $db->all();
-?>
-
-<html>
-<head>
-    <title>PHP-Apache-Mysql-Sample 示例 - DaoCloud</title>
-
-    <link href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            height: 100%;
-            color: #666;
-            display: table;
-        }
-
-        .container {
-            display: table-cell;
-            text-align: center;
-        }
-
-        .content {
-            text-align: center;
-            display: inline-block;
-        }
-
-
-    </style>
-</head>
-<body>
-<div class="container">
-    <div class="content">
-        <h1>
-            PHP-Apache-Mysql-Sample 示例
-        </h1>
-        <table class="table">
-            <caption>通讯录</caption>
-            <thead>
-            <tr>
-                <th>#</th>
-                <th>姓名</th>
-                <th>电话</th>
-                <th>操作</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php foreach ($contacts as $index => $contact) {
-                ?>
-                <tr>
-                    <th scope="row"> <?php echo $index + 1 ?></th>
-                    <td><?php echo $contact['name'] ?></td>
-                    <td><?php echo $contact['phone'] ?></td>
-                    <td>
-                        <a href="index.php?delete=<?php echo $contact['id'] ?>">删除</a>
-                    </td>
-                </tr>
-            <?php
-            } ?>
-            </tbody>
-        </table>
-
-        <div class="text-left">
-            <form method="post">
-                <div class="form-group">
-                    <label for="name">姓名</label>
-                    <input type="text" name="name" class="form-control" id="name" placeholder="姓名">
-                </div>
-                <div class="form-group">
-                    <label for="phone">号码</label>
-                    <input type="text" name="phone" class="form-control" id="phone" placeholder="号码">
-                </div>
-                <button type="submit" class="btn btn-success">新增</button>
-            </form>
-        </div>
-    </div>
-</div>
-</body>
-</html>
